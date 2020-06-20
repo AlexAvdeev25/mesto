6ad3b5155d0e563d74e9e10db0c7e8cf76cc0afb
AlexAvdeev <alexavdeev25wallet@yandex.ru>
2020-06-20 19:41:45 +0300

    Mesto commit3

--- script/index.js
+++ script/index.js
@@ -1,30 +1,29 @@
 let popup = document.querySelector('.popup');
 let openButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__save-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', closePopup);
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#name');
-let jobInput = document.querySelector('#job');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
 }
 
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
